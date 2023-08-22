2406b01c73bd3d8159b0647167036a2ae997ec31
ANORA\anjana <anjana@anorasolutions.com>
2023-08-22 14:21:33 +0530

    Revert "temperature conversion"
    This reverts commit 27bb2de22283d0dfe8c3657137f276f164514821.

--- celsius_conversion.py
+++
@@ -1,5 +0,0 @@
-temperature = list(map(int, input("Enter the temperature values in celsius : ").split()))
-
-Fahrenheit = [(lambda t: t * 1.8 + 32)(celsius) for celsius in temperature]
-
-print("Fahrenheit values are : ", Fahrenheit)
